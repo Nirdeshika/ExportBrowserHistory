0c4029248ea0c32c006a9060f4465ba6b8d5e026
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-17 18:39:28 -0700

    - Created a popup that is shown for all the URLs except history. - Added code to download entire history on clicking Complete History button

--- ExportBrowserHistoryToExcel/popup.js
+++ ExportBrowserHistoryToExcel/popup.js
@@ -1,5 +1,14 @@
-chrome.tabs.query({"currentWindow" : true, "active" : true},function(tabs){
-	var tab = tabs[0];
-	chrome.runtime.sendMessage({"url" : tab.url},function(response){
-		console.log("from popup.js");
-		console.log(response);})});
+document.getElementById(1).addEventListener("click", function(){
+    chrome.history.search({"text" : "","maxResults" : 1000000, 
+    	"startTime" : new Date(1970,1,1).getTime()},function(results){
+    	var line = "";
+    	for (var i = 0; i < results.length; i++) {
+    		line += "\""+results[i].url + "\"" +"," + new Date(results[i].lastVisitTime) + "\r\n";
+    	}
+    	chrome.downloads.download({"url": "data:text/csv;charset=utf-8,"+encodeURI(line)});
+    });
+});
+
+document.getElementById(2).addEventListener("click", function(){
+    chrome.tabs.create({"url":"chrome://history"});
+});
