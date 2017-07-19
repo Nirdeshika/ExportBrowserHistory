6d17b294633dfddb40682fe4657ea14b0224ecbd
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-19 10:26:23 -0700

    Added a new pop up page which is displayed on clicking "Filtered History".

---
+++ ExportBrowserHistory/popupOnFilter.js
@@ -0,0 +1,11 @@
+document.getElementById("Download").addEventListener("click",function(){
+	console.log(document.getElementById("keyword").value);
+	chrome.history.search({"text" : document.getElementById("keyword").value,"maxResults" : 0, "startTime" : 0},function(results){
+    	var line = "URL, Last Visted On, Number of Times Visted\r\n\n";
+    	for (var i = 0; i < results.length; i++) {
+    		line += "\""+results[i].url + "\"" +"," + new Date(results[i].lastVisitTime) +"," 
+    		+ results[i].visitCount +"\r\n";
+    	}
+    	chrome.downloads.download({"url": "data:text/csv;charset=utf-8,"+encodeURI(line)});
+    });
+});
