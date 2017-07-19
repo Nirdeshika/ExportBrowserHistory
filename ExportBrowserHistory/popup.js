370d9d503485521a489d1062332ffa38f886c875
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-19 10:29:35 -0700

    Added headings to CSV file. Changed max results to 0 instead of a large number (https://bugs.chromium.org/p/chromium/issues/detail?id=73812)

--- ExportBrowserHistory/popup.js
+++ ExportBrowserHistory/popup.js
@@ -1,14 +1,14 @@
 document.getElementById(1).addEventListener("click", function(){
-    chrome.history.search({"text" : "","maxResults" : 1000000, 
-    	"startTime" : new Date(1970,1,1).getTime()},function(results){
-    	var line = "";
+    chrome.history.search({"text" : "","maxResults" : 0, "startTime" : 0},function(results){
+    	var line = "URL, Last Visted On, Number of Times Visted\r\n\n";
     	for (var i = 0; i < results.length; i++) {
-    		line += "\""+results[i].url + "\"" +"," + new Date(results[i].lastVisitTime) + "\r\n";
+    		line += "\""+results[i].url + "\"" +"," + new Date(results[i].lastVisitTime) +"," 
+    		+ results[i].visitCount +"\r\n";
     	}
     	chrome.downloads.download({"url": "data:text/csv;charset=utf-8,"+encodeURI(line)});
     });
 });
 
 document.getElementById(2).addEventListener("click", function(){
-    chrome.tabs.create({"url":"chrome://history"});
+    window.open("popupOnFilter.html","_self");
 });
