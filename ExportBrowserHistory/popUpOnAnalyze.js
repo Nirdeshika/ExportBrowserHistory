1b9c8fdb5549bd547aca8825ff7ebc691cf689c3
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-31 15:27:42 -0700

    Added code to "Analyze History" such that on clicking the slice of the pie chart downloads the corresponding history.

--- ExportBrowserHistory/popUpOnAnalyze.js
+++ ExportBrowserHistory/popUpOnAnalyze.js
@@ -3,8 +3,6 @@ var numberOfTimesSocialMediaWasVisited = 0;
 var numberOfTimesSearchWasVisited = 0;
 
 function createChart() {
-    console.log("from createChart" + numberOfTimesEducationalSitesWasVisited);
-
     var ctx = document.getElementById("history_analyze_pie_chart").getContext('2d');
     var chart = new Chart(ctx, {
         type: 'pie',
@@ -19,9 +17,24 @@ function createChart() {
             }]
         },
 
-        options: {}
+        options: {"onClick": pieSegmentOnClick, "events": ["click", "mousemove"]}
     });
 
+
+    function pieSegmentOnClick(event) {
+        var elementsAtTheEvent = chart.getElementsAtEvent(event);
+        if (elementsAtTheEvent.length > 0) {
+            var labelOfTheSlice = chart.data.labels[elementsAtTheEvent[0]._index];
+            if (labelOfTheSlice == "Educational")
+                downloadEducationalSites();
+            if (labelOfTheSlice == "Searching")
+                downloadSearch();
+            if (labelOfTheSlice == "Social Media") {
+                Promise.all([getSocialMediaHistory("facebook"), getSocialMediaHistory("instagram"), getSocialMediaHistory("twitter")]).
+                    then(function(){downloadSocialMedia()});
+            }
+        }
+    }
 }
 
 function getnumberOfTimesEducationalSitesWasVisitedPromise() {
@@ -62,7 +75,49 @@ function getNumberOfTimesSearchWasVisited() {
 
 Promise.all([getnumberOfTimesEducationalSitesWasVisitedPromise(), getNumberOfTimesSocialMediaWasVisited('facebook'),
     getNumberOfTimesSocialMediaWasVisited('instagram'), getNumberOfTimesSocialMediaWasVisited('twitter'),
-    getNumberOfTimesSearchWasVisited()]).then(function(){createChart()});
+    getNumberOfTimesSearchWasVisited()]).then(function () {
+    createChart()
+});
 
+function downloadEducationalSites() {
+    chrome.history.search({"text": "stackoverflow", "maxResults": 0, "startTime": 0}, function (results) {
+        for (var i = 0; i < results.length; i++) {
+            var line = "URL, Last Visited On, Number of Times Visited\r\n\n";
+            for (var i = 0; i < results.length; i++) {
+                line += "\"" + results[i].url + "\"" + "," + new Date(results[i].lastVisitTime) + ","
+                    + results[i].visitCount + "\r\n";
+            }
+            chrome.downloads.download({"url": "data:text/csv;charset=utf-8," + encodeURI(line)});
+        }
+    });
+}
 
+function downloadSocialMedia() {
+    chrome.downloads.download({"url": "data:text/csv;charset=utf-8," + encodeURI(socialMediaLine)});
+}
 
+function downloadSearch() {
+    chrome.history.search({"text": "google.com/", "maxResults": 0, "startTime": 0}, function (results) {
+        for (var i = 0; i < results.length; i++) {
+            line = "URL, Last Visited On, Number of Times Visited\r\n\n";
+            for (var i = 0; i < results.length; i++) {
+                line += "\"" + results[i].url + "\"" + "," + new Date(results[i].lastVisitTime) + ","
+                    + results[i].visitCount + "\r\n";
+            }
+            chrome.downloads.download({"url": "data:text/csv;charset=utf-8," + encodeURI(line)});
+        }
+    });
+}
+
+var socialMediaLine = "URL, Last Visited On, Number of Times Visited\r\n\n";
+
+function getSocialMediaHistory(keyword) {
+    return new Promise(function(resolve){
+        chrome.history.search({"text": keyword, "maxResults": 0, "startTime": 0}, function (results) {
+            for (var i = 0; i < results.length; i++) {
+                socialMediaLine += "\"" + results[i].url + "\"" + "," + new Date(results[i].lastVisitTime) + "," + results[i].visitCount + "\r\n";
+            }
+            resolve();
+        });
+    });
+}
